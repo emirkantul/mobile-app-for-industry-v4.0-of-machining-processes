48947bc2b355d5e4f3f62dca9c2ebfb0906f9db1
emirkantul <emirkantul@paratica.com>
2023-04-03 01:28:23 +0300

    finalized. High amount of data takes long time to change anything. also charts don't look as it should.

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,26 @@
+import 'dart:developer';
 import 'package:flutter/material.dart';
 import 'dart:async' show Future;
 import 'package:flutter/services.dart' show rootBundle;
-import 'package:syncfusion_flutter_charts/charts.dart';
-import 'package:flutter/material.dart';
+import 'session.dart';
 
-void main() => runApp(App());
+void main() => runApp(const App());
 
-class _vibrationData {
-  final double value;
-  final double time;
-
-  _vibrationData({required this.value, required this.time});
-}
-
-class _soundData {
-  final double value;
-  final double time;
-
-  _soundData({required this.value, required this.time});
-}
-
-class App extends StatelessWidget {
+/*
+  app-top-bar
+*/
+class App extends StatefulWidget {
   const App({super.key});
 
   @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Bottom Nav Demo',
+      title: 'Mobile App for Industry v4.0 Milling Process',
       theme: ThemeData(
         primarySwatch: const MaterialColor(
           0xFF2F495E,
@@ -46,9 +40,12 @@ class App extends StatelessWidget {
       ),
       home: const HomePage(title: 'Past Sessions'),
     );
-		}
+  }
 }
 
+/*
+  home-page
+*/
 class HomePage extends StatefulWidget {
   const HomePage({Key? key, required this.title}) : super(key: key);
 
@@ -59,87 +56,97 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  int _selectedIndex = 0;
-  Map<String, Map<String, double>> _dataList = {};
+  /*
+    initializations
+  */
+  int _selectedTabIndex = 0;
   bool _isLoading = true;
-  String _selectedDropdownValue = '1';
-
+  Map<String, List<TSV>> _tsvData = {};
   static const List<String> _appBarTitles = <String>[
     'Past Sessions',
     'Last Session',
   ];
 
-  List<Map<String, double>> _vibrationData = [];
-  List<Map<String, double>> _soundData = [];
-
+  /*
+    init-state
+  */
   @override
   void initState() {
     super.initState();
-    loadData();
+    _loadData();
   }
 
-  Future<void> loadData() async {
+  /*
+    load-static-data
+  */
+  Future<void> _loadData() async {
     try {
+      _isLoading = true;
+
       // Read multiple files using rootBundle
       List<String> fileNames = [
         'AL7075_chatter_sound_vibro_t_001.lvm',
         'AL7075_chatter_sound_vibro_t_002.lvm',
-        'AL7075_chatter_sound_vibro_t_003.lvm',
-        'AL7075_chatter_sound_vibro_t_004.lvm',
-        'AL7075_chatter_sound_vibro_t_005.lvm',
-        'AL7075_chatter_sound_vibro_t_006.lvm',
-        'AL7075_chatter_sound_vibro_t_007.lvm',
-        'AL7075_chatter_sound_vibro_t_008.lvm',
-        'AL7075_chatter_sound_vibro_t_009.lvm',
-        'AL7075_chatter_sound_vibro_t_010.lvm',
-        'AL7075_chatter_sound_vibro_t_011.lvm',
-        'AL7075_chatter_sound_vibro_t_012.lvm',
-        'AL7075_chatter_sound_vibro_t_013.lvm',
-        'AL7075_chatter_sound_vibro_t_014.lvm',
-        'AL7075_chatter_sound_vibro_t_015.lvm',
-        'AL7075_chatter_sound_vibro_t_016.lvm',
-        'AL7075_chatter_sound_vibro_t_017.lvm',
-        'AL7075_chatter_sound_vibro_t_018.lvm',
-        'AL7075_chatter_sound_vibro_t_019.lvm',
-        'AL7075_chatter_sound_vibro_t_020.lvm',
-        'AL7075_chatter_sound_vibro_t_021.lvm',
-        'AL7075_chatter_sound_vibro_t_022.lvm',
-        'AL7075_chatter_sound_vibro_t_023.lvm',
-        'AL7075_chatter_sound_vibro_t_024.lvm',
-        'AL7075_chatter_sound_vibro_t_025.lvm',
-        'AL7075_chatter_sound_vibro_t_026.lvm',
-        'AL7075_chatter_sound_vibro_t_027.lvm',
-        'AL7075_chatter_sound_vibro_t_028.lvm',
-        'AL7075_chatter_sound_vibro_t_029.lvm'
+        // 'AL7075_chatter_sound_vibro_t_003.lvm',
+        // 'AL7075_chatter_sound_vibro_t_004.lvm',
+        // 'AL7075_chatter_sound_vibro_t_005.lvm',
+        // 'AL7075_chatter_sound_vibro_t_006.lvm',
+        // 'AL7075_chatter_sound_vibro_t_007.lvm',
+        // 'AL7075_chatter_sound_vibro_t_008.lvm',
+        // 'AL7075_chatter_sound_vibro_t_009.lvm',
+        // 'AL7075_chatter_sound_vibro_t_010.lvm',
+        // 'AL7075_chatter_sound_vibro_t_011.lvm',
+        // 'AL7075_chatter_sound_vibro_t_012.lvm',
+        // 'AL7075_chatter_sound_vibro_t_013.lvm',
+        // 'AL7075_chatter_sound_vibro_t_014.lvm',
+        // 'AL7075_chatter_sound_vibro_t_015.lvm',
+        // 'AL7075_chatter_sound_vibro_t_016.lvm',
+        // 'AL7075_chatter_sound_vibro_t_017.lvm',
+        // 'AL7075_chatter_sound_vibro_t_018.lvm',
+        // 'AL7075_chatter_sound_vibro_t_019.lvm',
+        // 'AL7075_chatter_sound_vibro_t_020.lvm',
+        // 'AL7075_chatter_sound_vibro_t_021.lvm',
+        // 'AL7075_chatter_sound_vibro_t_022.lvm',
+        // 'AL7075_chatter_sound_vibro_t_023.lvm',
+        // 'AL7075_chatter_sound_vibro_t_024.lvm',
+        // 'AL7075_chatter_sound_vibro_t_025.lvm',
+        // 'AL7075_chatter_sound_vibro_t_026.lvm',
+        // 'AL7075_chatter_sound_vibro_t_027.lvm',
+        // 'AL7075_chatter_sound_vibro_t_028.lvm',
+        // 'AL7075_chatter_sound_vibro_t_029.lvm'
       ];
-      Map<String, Map<String, double>> data = {};
+      Map<String, List<TSV>> allTsv = {};
+      List<TSV> tsvs = [];
 
       for (var i = 0; i < fileNames.length; i++) {
         String content =
             await rootBundle.loadString('assets/data/${fileNames[i]}');
         List<String> lines = content.split('\n');
+
         for (var j = 0; j < lines.length; j++) {
-          List<String> tsv = lines[i].split('\t');
-          data[i.toString()] = {
-            't': double.parse(tsv[0].replaceAll(',', '.')),
-            's': double.parse(tsv[1].replaceAll(',', '.')),
-            'v': double.parse(tsv[2].replaceAll(',', '.')),
-          };
+          List<String> tsvLine = lines[i].split('\t');
+          tsvs.add(TSV(
+            time: double.parse(tsvLine[0].replaceAll(',', '.')),
+            sound: double.parse(tsvLine[1].replaceAll(',', '.')),
+            vibration: double.parse(tsvLine[2].replaceAll(',', '.')),
+          ));
         }
+
+        allTsv["$i-xxxxxxxx"] = tsvs; // TODO: add session date
       }
 
       setState(() {
-        _dataList = data;
         _isLoading = false;
+        _tsvData = allTsv;
       });
     } catch (error) {
-      print(error);
+      log("Error on load-static-data: $error");
     }
   }
 
-  void _onItemTapped(int index) {
+  void _onTabTapped(int index) {
     setState(() {
-      _selectedIndex = index;
+      _selectedTabIndex = index;
     });
   }
 
@@ -148,104 +155,20 @@ class _HomePageState extends State<HomePage> {
   */
   @override
   Widget build(BuildContext context) {
-    // ignore: unused_local_variable
-    Widget bodyWidget;
-
-    if (_isLoading) {
-      bodyWidget = const Center(
-        child: CircularProgressIndicator(),
-      );
-    } else {
-      bodyWidget = Center(
-        child: Text(_selectedIndex.toString()),
-      );
-    }
-
-    _dataList.forEach((key, value) {
-      _vibrationData
-          .add({"time": value["t"] ?? 0.0, "vibration": value["v"] ?? 0.0});
-      _soundData.add({"time": value["t"] ?? 0.0, "sound": value["s"] ?? 0.0});
-    });
-
     return Scaffold(
       appBar: AppBar(
         title: Align(
           alignment: Alignment.centerLeft,
-          child: Text(_appBarTitles[_selectedIndex]),
+          child: Text(_appBarTitles[_selectedTabIndex]),
         ),
-        actions: <Widget>[
-          /*
-            dropdown-widget
-          */
-          DropdownButton<String>(
-            value: _selectedDropdownValue,
-            onChanged: 
-              (String? newValue, List<Map<String, double>> newVibration,  List<Map<String, double>> newSound) {
-                  setState(() {
-                    _selectedDropdownValue = newValue!;
-                    _vibrationData = newVibration;
-                    _soundData = newSound;
-                  }));
-            items: _dataList.keys
-                .toList()
-                .map<DropdownMenuItem<String>>((String value) {
-              return DropdownMenuItem<String>(
-                value: value,
-                child: Text(value),
-              );
-            }).toList(),
-          ),
-        ],
       ),
-      /*
-        charts
-      */
-      body: Column(
-        children: [
-          /*
-          vibration
-        */
-          Expanded(
-            child: SfCartesianChart(
-              title: ChartTitle(text: "Vibration/Time"),
-              legend: Legend(isVisible: true),
-              series: <ChartSeries>[
-                LineSeries<Map<String, double>, double>(
-                  dataSource: _vibrationData,
-                  xValueMapper: (Map<String, double> data, _) => data["time"],
-                  yValueMapper: (Map<String, double> data, _) =>
-                      data["vibration"],
-                  name: "Vibration",
-                ),
-              ],
-              primaryXAxis: NumericAxis(),
-              primaryYAxis: NumericAxis(),
+      body: _isLoading
+          ? const Center(
+              child: CircularProgressIndicator(),
+            )
+          : Session(
+              tsvData: _tsvData,
             ),
-          ),
-          /*
-          sound
-        */
-          Expanded(
-            child: SfCartesianChart(
-              title: ChartTitle(text: "Sound/Time"),
-              legend: Legend(isVisible: true),
-              series: <ChartSeries>[
-                LineSeries<Map<String, double>, double>(
-                  dataSource: _soundData,
-                  xValueMapper: (Map<String, double> data, _) => data["time"],
-                  yValueMapper: (Map<String, double> data, _) => data["sound"],
-                  name: "Sound",
-                ),
-              ],
-              primaryXAxis: NumericAxis(),
-              primaryYAxis: NumericAxis(),
-            ),
-          ),
-        ],
-      ),
-      /*
-        navigation-bar
-      */
       bottomNavigationBar: BottomNavigationBar(
         items: const <BottomNavigationBarItem>[
           BottomNavigationBarItem(
@@ -257,8 +180,8 @@ class _HomePageState extends State<HomePage> {
             label: 'Last Session',
           ),
         ],
-        currentIndex: _selectedIndex,
-        onTap: _onItemTapped,
+        currentIndex: _selectedTabIndex,
+        onTap: _onTabTapped,
       ),
     );
   }
---
+++ lib/session.dart
@@ -0,0 +1,118 @@
+import 'dart:developer';
+import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
+
+/*
+  data-types
+*/
+class TSV {
+  final double time;
+  final double sound;
+  final double vibration;
+
+  TSV({required this.time, required this.sound, required this.vibration});
+}
+
+class Session extends StatefulWidget {
+  Map<String, List<TSV>> tsvData = {};
+
+  Session({
+    super.key,
+    required this.tsvData,
+  });
+
+  @override
+  State<Session> createState() => _SessionState();
+}
+
+class _SessionState extends State<Session> {
+  bool _isLoading = true;
+  String _selectedDropdownValue = '0-xxxxxxxx';
+  List<TSV> _chartData = [];
+
+  /*
+    init-state
+  */
+  @override
+  void initState() {
+    super.initState();
+    _updateChartData('0-xxxxxxxx'); // Load initial data for session 0
+  }
+
+  /* 
+    update-tsv-data
+  */
+  void _updateChartData(String selectedDropdownValue) {
+    setState(() {
+      _chartData = widget.tsvData[selectedDropdownValue]!;
+      _isLoading = false;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        actions: <Widget>[
+          DropdownButton<String>(
+            value: _selectedDropdownValue,
+            onChanged: (String? newValue) {
+              setState(() {
+                _selectedDropdownValue = newValue!;
+              });
+              // updateChartData(_selectedDropdownValue);
+            },
+            items: widget.tsvData.keys.toList().map<DropdownMenuItem<String>>(
+              (String value) {
+                return DropdownMenuItem<String>(
+                  value: value,
+                  child: Text(value),
+                );
+              },
+            ).toList(),
+          ),
+        ],
+      ),
+      body: _isLoading
+          ? const Center(
+              child: CircularProgressIndicator(),
+            )
+          : Column(
+              children: [
+                Expanded(
+                  child: SfCartesianChart(
+                    title: ChartTitle(text: "Vibration/Time"),
+                    legend: Legend(isVisible: true),
+                    series: <ChartSeries>[
+                      LineSeries<TSV, double>(
+                        dataSource: _chartData,
+                        xValueMapper: (TSV data, _) => data.time,
+                        yValueMapper: (TSV data, _) => data.vibration,
+                        name: "Vibration",
+                      ),
+                    ],
+                    primaryXAxis: NumericAxis(),
+                    primaryYAxis: NumericAxis(),
+                  ),
+                ),
+                Expanded(
+                  child: SfCartesianChart(
+                    title: ChartTitle(text: "Sound/Time"),
+                    legend: Legend(isVisible: true),
+                    series: <ChartSeries>[
+                      LineSeries<TSV, double>(
+                        dataSource: _chartData,
+                        xValueMapper: (TSV data, _) => data.time,
+                        yValueMapper: (TSV data, _) => data.sound,
+                        name: "Sound",
+                      ),
+                    ],
+                    primaryXAxis: NumericAxis(),
+                    primaryYAxis: NumericAxis(),
+                  ),
+                ),
+              ],
+            ),
+    );
+  }
+}
